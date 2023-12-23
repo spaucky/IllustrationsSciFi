0fa2330e50baafcb6c1cbf8c705e3f773195e4a5
Jasper Kaucky <jasperkaucky@icloud.com>
2023-12-23 22:56:23 +0100

    Manually Removing Images

--- DuplicateCheck.py
+++ DuplicateCheck.py
@@ -3,8 +3,9 @@ import os
 
 hashes = set()
 
-files_before = next(os.walk('PlantArt'))[2] #directory is your directory path as string
+files_before = next(os.walk('PlantArt'))[2] #the folder name can be changed here and after
 
+#runs through the images and if the image has an unrecognised hash - adds it to a set of hashes which future images are checked against
 for filename in os.listdir('PlantArt'):
     path = os.path.join('PlantArt', filename)
     digest = hashlib.sha1(open(path,'rb').read()).digest()
@@ -15,4 +16,5 @@ for filename in os.listdir('PlantArt'):
         
 files_after = next(os.walk('PlantArt'))[2] #directory is your directory path as string
 
-assert files_before == files_after
+#check that there is the same number of images before and after
+assert len(files_before) == len(files_after)
